56aebadd4e38f9c15ed183433017c9762c697f42
Seth NeJame <sethnejame@gmail.com>
2019-07-29 20:31:15 +0200

    Users are now appended to the current list of github cards upon 'add card'

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const testData = [
 const CardList = (props) => (
   <div>
     {props.profiles.map(profile => {
-      return <Card {...profile} />;
+      return <Card key={profile.id} {...profile} />;
     })}
   </div>
 );
@@ -23,7 +23,7 @@ class Card extends React.Component {
     const profile = this.props;
     return (
       <div className="github-profile" >
-        <img src={profile.avatar_url} />
+        <img src={profile.avatar_url} alt={profile.name}/>
         <div className="info" >
           <div className="name" >{profile.name}</div>
           <div className="company" >{profile.company}</div>
@@ -35,9 +35,11 @@ class Card extends React.Component {
 
 class Form extends React.Component {
   state = { userName: '' };
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(this.state.userName)
+    const resp = await axios.get(`https://api.github.com/users/${this.state.userName}`)
+    this.props.onSubmit(resp.data);
+    this.setState({ userName: '' });
   }
   render() {
     return (
@@ -60,12 +62,17 @@ class App extends React.Component {
   state = {
     profiles: testData,
   };
+  addNewProfile = (profileData) => {
+    this.setState(prevState => ({
+      profiles: [...prevState.profiles, profileData],
+    }));
+  };
 
   render() {
     return (
       <div>
         <div className="header">{this.props.title}</div>
-          <Form />
+          <Form onSubmit={this.addNewProfile} />
           <CardList profiles={this.state.profiles} />
         </div>
     );
